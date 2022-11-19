ad15225a860c562805e3c72515a9a060dbdb70db
Tumpeti <petertumpek20@gmail.com>
2022-11-19 11:25:00 +0100

    Órai munka fájl

--- main.py
+++ main.py
@@ -18,6 +18,62 @@ while i < len(homerseklet):
     elozonap_homerseklete = homerseklet[i-1]
 print(f"Októberben a {csokkent_a_homerseklet} napokon csökkent a hőmérséglet három vagy több mint három fokkal.")
 """
+"""
+# szövegkezelés
+# 1
+szoveg = input("Kérek egy tetszőleges szöveget: ")
+szoveg = szoveg.upper()
+print(szoveg)
+# 2
+if len(szoveg) > 10:
+    print(f"A szöveg hossza {len(szoveg)} karakter.")
+"""
+# 3
+"""
 
+szoveg2 = input("Kérek egy legalább három betűs szöveget!")
+szoveg2 = szoveg2.upper()
+if len(szoveg2) < 3:
+    print("Nem elég hosszú a szöveg!")
+"""
+# 4
+"""print("Az első a helye: ")
+print(szoveg2.find("a"))
+print("Az összes a száma: ")
+print(szoveg2.count("a"))
+"""
+"""
+i = 0
+while i < len(szoveg2):
+    if szoveg2[i] == "A":
+        print(f"A {i+1}. helyen áll az első a betű.")
+        break
+    else: i += 1
+"""
+# Zsolt variációja:
 
+szoveg_jo = False
+while not szoveg_jo:
+    szoveg = input("Adjon meg egy legalább hárombetűs szót! ")
+    if len(szoveg) >= 3:
+        szoveg_jo = True
+
+# 5
+i = 0
+osszes_a = 0
+while i < len(szoveg):
+    if szoveg.upper()[i] == "A":
+        osszes_a += 1
+    i += 1
+print(f"Az összes a betű száma: {osszes_a}")
+# 1
+
+nevek_lista = ""
+egy_nev = 0
+while egy_nev != "@":
+    egy_nev = (input("Kérek neveket: "))
+    if egy_nev == "@":
+        break
+    nevek_lista += egy_nev + " "
+print(nevek_lista)
 
